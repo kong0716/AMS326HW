b629e97cf5fc3e061dc6e4ee81e1edddae02a9bd
Darren Kong <darren.kong@stonybrook.edu>
2020-02-14 15:48:44 -0500

    Added the fpops calc

--- hw1pt1.py
+++ hw1pt1.py
@@ -4,30 +4,23 @@
 # Problem1.1
 import math
 import random
-
+fpops = 0
 # Functions taken from problem, creates a half heart together
 def f_(x):
     # Bounds are from 0 to sqrt(2)
+    global fpops
+    fpops += 1
     return math.sqrt(2-math.pow(x,2)) + math.sqrt(x)
 
 def h_(x):
     # Bounds are from 0 to sqrt(2)
+    global fpops
+    fpops += 2
     return -math.sqrt(2-math.pow(x,2)) + math.sqrt(x)
-    
-def trapazoidal_rule(n, a, b):
-    trapArea = 0
-    a1 = a
-    b1 = a+n
-    while b1 <= b:
-        trapArea += ((f_(a1) + f_(b1))/2)*(b1-a1)
-        a1 += n
-        b1 += n
-    return trapArea
-#print(crude_montecarlo(100000000, 0, math.sqrt(2), 0, 2.1))
-#print(trapazoidal_rule(.000001, 0, math.sqrt(2)))
 
 def heartArea(n):
     # Area above the x-axis
+    global fpops
     trapArea = 0
     a1 = 0
     b1 = n
@@ -36,13 +29,16 @@ def heartArea(n):
         trapArea += ((f_(a1) + f_(b1))/2)*(b1-a1)
         a1 += n
         b1 += n
+        fpops += 7
     a1 = 1
     b1 = 1 + n
+    fpops += 1
     while b1 <= math.sqrt(2):
         #subtract since it is the area under the curve and we want to subtract that from previous area
         trapArea -= ((h_(a1) + h_(b1))/2)*(b1-a1)
         a1 += n
         b1 += n
+        fpops += 7
     # Area below the x-axis
     a1 = 0
     b1 = n
@@ -51,16 +47,21 @@ def heartArea(n):
         trapArea -= ((h_(a1) + h_(b1))/2)*(b1-a1)
         a1 += n
         b1 += n
+        fpops += 7
     # Double the area for the other half of the heart
+    fpops += 1
     return trapArea*2.0
 heart = heartArea(math.pow(10, -5))
 print("Area of the heart is " + str(heart))
 
 #Calculate the area of the max circle enclosed by the heart
 def bottomCircle(x, r):
+    global fpops
+    fpops += 4
     return -math.sqrt(math.pow(r,2)-math.pow(x,2)) - r + math.sqrt(2)
 
 def intersectionTest(x1, x2, r, increments, epsilon):
+    global fpops
     interectionctr = 0
     while x1 <= x2:
         x1 += increments
@@ -68,22 +69,30 @@ def intersectionTest(x1, x2, r, increments, epsilon):
             break
         if abs(bottomCircle(x1, r) - h_(x1)) <= epsilon:
             interectionctr += 1
+            fpops += 1
+        fpops += 2
     return interectionctr
 
 
 def findmaxcircleRadius(minr, maxr, increments, epsilon):
+    global fpops
     #Crude binary search
     while abs(maxr) - abs(minr) >= epsilon:
         r = (maxr + minr)/2
+        fpops += 2
         if intersectionTest(0, maxr, r, increments, epsilon) == 1:
             return r
         if intersectionTest(0, maxr, r, increments, epsilon) < 1:
             minr = r+increments
+            fpops += 1
         if intersectionTest(0, maxr, r, increments, epsilon) > 1:
             maxr = r-increments
+            fpops += 1
+        fpops += 1
         #print("MaxR = " + str(maxr) + " R = " + ", " + "MinR = " + str(minr))
     #Approcximation rounding errors
     return r
 circleArea = math.pow(findmaxcircleRadius(0, math.sqrt(2), math.pow(10, -5), math.pow(10, -5)),2)*math.pi
 print("Area of the circle is " + str(circleArea))
 print("Area of the remaining heart is " + str(heart-circleArea))
+print("Floating Point Operations " + str(fpops))
