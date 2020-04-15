73047d37441eae6e560707698cbe4f6966bef236
Darren Kong <darren.kong@stonybrook.edu>
2020-04-14 20:13:37 -0400

    Finished the inital RK4 function for pt1, need to clean up and formatting

---
+++ hw3pt1.py
@@ -0,0 +1,80 @@
+# Darren Kong
+# 110770716
+# AMS326
+import matplotlib.pyplot as plt
+import math
+
+v_0 = 14
+v_B1 = 7
+v_B2 = 14
+v_B3 = 21
+a = 7777
+
+#Runge Kutta Method
+def w(x):
+    return 4*v_0*((x/a) - ((x*x)/(a*a)))
+
+def f(x, y, vb):
+    return (y/x) - (w(x)/(vb*(x/math.sqrt((x*x) + (y*y)))))
+
+def runge_kutta(f, x0, y0, h, vb):#Anything after h is other constants or variables not explicit in the original RK4
+    k1 = f(x0, y0, vb)
+    k2 = f(x0 + (.5*h), y0 + (.5*h*k1), vb)
+    k3 = f(x0 + (.5*h), y0 + (.5*h*k2), vb)
+    k4 = f(x0 + h, y0 + (h*k3), vb)
+    tn1 = x0 + h
+    yn1 = y0 + ((1/6)*h*(k1 + k2 + k3 + k4))
+    return tn1 , yn1
+xplot = list()
+yplot = list()
+x = 7777
+y = 0
+h = -.1
+while x > 0:
+    xplot.append(x)
+    yplot.append(y)
+    x, y = runge_kutta(f, x, y, h, v_B1)
+plt.scatter(xplot, yplot)
+plt.show()
+'''
+def g(k, A, T):
+    return k*(A-(T**(.999)))
+
+def runge_kutta(g, x0, y0, h, k):
+    k1 = g(k, x0, y0)
+    k2 = g(k, x0 + (.5*h), y0 + (.5*h*k1))
+    k3 = g(k, x0 + (.5*h), y0 + (.5*h*k2))
+    k4 = g(k, x0 + h, y0 + (h*k3))
+    tn1 = x0 + h
+    yn1 = y0 + ((1/6)*h*(k1 + k2 + k3 + k4))
+    return tn1 , yn1
+
+def findk(mins, maxs, increments, epsilon):
+    b = increments
+    xplot = list()
+    yplot = list()
+    while abs(maxs) - abs(mins) >= epsilon:
+        xplot = list()
+        yplot = list()
+        x = 0
+        y = 1
+        h = .01
+        #T_180 = 55.55
+        b = (maxs + mins)/2
+        while x < 180:
+            xplot.append(x)
+            yplot.append(y)
+            x, y = runge_kutta(g, x, y, h, b)
+        if yplot[len(yplot)-1] - 55.55 == 0:
+            return b
+        if yplot[len(yplot)-1] - 55.55 < epsilon:
+            mins = b+increments
+        if yplot[len(yplot)-1] - 55.55 > epsilon:
+            maxs = b-increments
+        print(b)
+    plt.scatter(xplot, yplot)
+    plt.show()
+    return b
+
+findk(0, 100, 10**(-10), 10**(-10))
+'''
